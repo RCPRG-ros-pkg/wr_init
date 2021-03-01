f9c61ca98619bfb3e701a0e34bf30cfb5d80304a
Wojciech Dudek <wojciechsbox@gmail.com>
2021-03-01 14:33:47 +0100

    adds installation scripts

---
+++ siu-wr-packages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt install ros-melodic-imu-sensor-controller ros-melodic-force-torque-sensor-controller ros-melodic-twist-mux ros-melodic-four-wheel-steering-controller ros-melodic-image-proc ros-melodic-range-sensor-layer ros-melodic-pointcloud-to-laserscan ros-melodic-laser-filters ros-melodic-teb-local-planner ros-melodic-gmapping ros-melodic-octomap-server ros-melodic-octomap-mapping ros-melodic-people-msgs ros-melodic-rosbridge-server ros-melodic-rospy-message-converter ros-melodic-sound-play ros-melodic-object-recognition-msgs ros-melodic-moveit-ros-planning-interface ros-melodic-move-base-msgs bison flex ros-melodic-navigation ros-melodic-moveit-planners-ompl python-scipy python-catkin-tools ros-melodic-turtlebot3-description
+sudo snap install --classic code
